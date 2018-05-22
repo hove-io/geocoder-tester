29fe27d14372d1544ca6ee5bb64ebfedf3726aa0
Adrien Matissart <a.matissart@qwantresearch.com>
2018-05-22 18:36:28 +0200

    Keep python 3.5 compatibility

--- allure/tasks.py
+++ allure/tasks.py
@@ -7,19 +7,24 @@ from os import path
 def generate_report(ctx, results_dir):
     print('Generating Allure report...')
     with ctx.cd('allure'):
-        if path.exists(f'{ctx.cwd}/allure-report/history'):
-            ctx.run(f'mv allure-report/history {results_dir}')
-        ctx.run(f'allure generate {results_dir} --clean')
+        if path.exists('{}/allure-report/history'.format(ctx.cwd)):
+            ctx.run('mv allure-report/history {}'.format(results_dir))
+        ctx.run('allure generate {} --clean'.format(results_dir))
 
 
 @task(default=True)
 def run_tests(ctx):
     dt_now = datetime.utcnow().isoformat()[:19]
-    results_dir = f'res_{dt_now}'
+    results_dir = 'res_{}'.format(dt_now)
 
-    ctx.run(
-        f'pytest {ctx.tests_files} --api-url {ctx.api_url} '
-        f'--loose-compare --tb=short --alluredir allure/{results_dir}',
+    ctx.run("""
+        pytest {tests_files} --api-url {api_url} \
+        --loose-compare --tb=short --alluredir allure/{results_dir}
+        """.format(
+            tests_files=ctx.tests_files,
+            api_url=ctx.api_url,
+            results_dir=results_dir
+        ),
         warn=True
     )
 
